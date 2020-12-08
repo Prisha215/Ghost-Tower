536b71044f02f17f9deb47e153ae06c9b52a9beb
Prisha215 <74864350+Prisha215@users.noreply.github.com>
2020-12-08 19:54:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+//creating variables
+var climber,climberimage ;
+var ghost,ghostimage ;
+var tower,towerimage;
+var door ,doorimage;
+//creating fuction preload
+function preload(){ climberimage=loadImage("climber.png");
+ghostimage=loadImage("ghost-standing.png")  ;
+towerimage=loadImage("tower.png"); doorImage=loadImage("door.png");
+  
+}
+function setup (){
+ createCanvas(600,600) ;
+tower=createSprite(150,100,20,10);
+ tower.addImage 
+}
